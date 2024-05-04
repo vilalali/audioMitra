335b642776854514d254dc30e18c7194de43e5ee
vilalali <belaal.ali@gmail.com>
2024-05-04 23:57:27 +0530

    New feture added

--- audioMitra-frontend/src/cred.js
+++ audioMitra-frontend/src/cred.js
@@ -1,6 +1,6 @@
 //API base URL
-const API_URL = 'http://10.1.65.64:8002';
-//const API_URL = 'http://10.2.138.112:8002';
+//const API_URL = 'http://10.1.65.64:8002';
+const API_URL = 'http://10.2.138.112:8002';
 
 // Export the API_URL
 export { API_URL };
